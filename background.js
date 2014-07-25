24bb80c1273ae64b7ae8d9440c8b71d743384a94
alexfreska <alex.freska@gmail.com>
2014-07-25 15:25:48 -0500

    init

---
+++ background.js
@@ -0,0 +1,235 @@
+// colors
+UP      = '#33cc66';
+STAY    = 'blue';
+DOWN    = '#ff2741';
+ERROR   = 'black';
+
+// pixel measures
+var w = 15;
+var h = 10;
+
+// starting pixel for each positon used for 1,7,30 day icons
+var POS1 = 4,
+    POS2 = 9,
+    POS3 = 14;
+
+// week is actually 3 days
+var week = 3 * 24 * 60 * 60;
+var day = 24 * 60 * 60;
+var fif = 15 * 60;
+
+// query used to retrieve latest price (15min delay)
+var queryLatest = 'https://blockchain.info/ticker?cors=true';
+
+// var queryLatest = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+
+// query used to return 1 day, 7 day, and 30 day averages in USD (returns all currencies)
+var queryAverages = 'http://api.bitcoincharts.com/v1/weighted_prices.json';
+
+var queryHistory = 'https://api.coindesk.com/v1/bpi/historical/close.json';
+
+function Bitcoins () {
+    var s = this;
+
+    var data = {
+        hour: {
+            average: 0,
+            change: 0
+        },
+        day: {
+            average: 0,
+            change: 0
+        },
+        week: {
+            average: 0,
+            change: 0
+        },
+        month: {
+            average: 0,
+            change: 0
+        },
+        currentPrice: -1
+    }
+
+    s.getPrices = function () {
+        return data.currentPrice != -1 ? data : -1;
+    }
+
+    // updates 1,7 and 30 day averages
+    s.updateAverages = function (callback) {
+
+        $.get(queryAverages,function(newData) {
+
+            console.log('updated averages.');
+
+            var usd = $.parseJSON(newData)['USD'];
+
+            data.day.average = usd['24h'];
+            data.week.average = usd['7d'];
+            data.month.average = usd['30d'];
+
+            // if callback provided, execute it
+            if(typeof callback != 'undefined') callback();
+
+        });
+    }
+
+    // updates the historical data used for graphing
+    s.queryHistory = function (callback) {
+        console.log('queryHistory');
+        // JSON response is incorrectly formatted therefore I have to
+        // handle error case and reparse the responseText manuallyk
+        $.get(queryHistory, function (newData) {
+
+            console.log('queryHistory/succ');
+            console.log(newData);
+
+        }).fail( function (err) {
+            console.log('queryHistory/err');
+            console.log(err.error());
+
+            var res = err.responseText;
+
+            res2 = JSON.parse(res);
+            console.log(res2);
+            res = res.split('{"bpi":{')[1];
+            res = res.split('},"disclaimer"')[0];
+            res = res.split(',');
+
+            var obj = {
+                values: []
+            };
+            res.forEach( function (ea) {
+                var item = {};
+                var kv = ea.split(':');
+                item.x = new Date(kv[0].slice(1,kv[0].length-1)).getTime()/1000;
+                item.y = parseInt(kv[1]);
+                obj.values.push(item);
+            });
+
+            callback(obj);
+        });
+
+
+    }
+
+    // gets latest price (delayed 15min)
+    s.updateCurrent = function () {
+
+        $.get(queryLatest,function(newData) {
+
+            console.log('updated latest.');
+            console.log(newData);
+
+            newCurrent(newData);
+
+        }).fail(function(err) {
+
+             var res = err.responseText;
+
+             var r1 = res.slice(0,res.length - 6);
+             var r2 = res.slice(res.length - 5, res.length);
+
+             res = r1 + r2;
+
+             var newData = JSON.parse(res);
+
+             newCurrent(newData);
+         });
+    }
+
+    function newCurrent(newData) {
+        // data.currentPrice = data['bpi']['USD']['rate'];
+        data.currentPrice = newData['USD']['15m'];
+
+        data.day.change     = data.currentPrice - data.day.average;
+        data.week.change    = data.currentPrice - data.week.average;
+        data.month.change   = data.currentPrice - data.month.average;
+
+        drawIcon(data);
+    }
+};
+
+// redraw icon with updated price information
+function drawIcon(updates) {
+
+    var canvas  = document.getElementById('canvas');
+    var ctx     = canvas.getContext('2d');
+
+    ctx.clearRect(0,0,50,50);
+    ctx.rect(0,0,50,50);
+    ctx.fillStyle = "rgba(3,30,3,0.8)";
+    ctx.fill();
+    ctx.stroke();
+
+    // draw to the canvas...
+    updates.day.change < 0 ?    down(ctx,1) : up(ctx,1);
+    updates.week.change < 0 ?   down(ctx,2) : up(ctx,2);
+    updates.week.change < 0 ?   down(ctx,3) : up(ctx,3);
+
+    // round to integer for badge price
+    var price = updates.currentPrice.toFixed(0);
+
+    // if the number if 4 digits or more use smaller font px
+    ctx.font = price.toString().length > 3 ? "8px Helvetica Neue" : "10px Helvetica Neue";
+
+    // set badge using price info
+    chrome.browserAction.setBadgeBackgroundColor({ color: UP });
+    chrome.browserAction.setBadgeText({text: updates.currentPrice.toFixed(0).toString()});
+
+    // set icon using canvas image
+    var imageData = ctx.getImageData(0, 0, 19, 19);
+    chrome.browserAction.setIcon({
+        imageData: imageData
+    });
+
+}
+// draws icon used for price increase
+function up(ctx,pos) {
+
+    var x = pos == 1? POS1 : pos == 2? POS2 : POS3;
+    ctx.fillStyle = UP;
+    ctx.beginPath();
+    ctx.moveTo(x,h);
+    ctx.lineTo(x,h-7);
+    ctx.lineTo(x+2,h-7);
+    ctx.lineTo(x+2,h);
+    ctx.closePath();
+    ctx.fill();
+
+}
+
+// draws icon used for price decrease
+function down(ctx,pos) {
+
+    var x = pos == 1? POS1 : pos == 2? POS2 : POS3;
+    ctx.fillStyle = DOWN;
+    ctx.beginPath();
+    ctx.moveTo(x,h);
+    ctx.lineTo(x,h-7);
+    ctx.lineTo(x+2,h-7);
+    ctx.lineTo(x+2,h);
+    ctx.closePath();
+    ctx.fill();
+
+}
+
+// init
+bitcoins = new Bitcoins();
+
+
+// initialize
+bitcoins.updateAverages(bitcoins.updateCurrent);
+
+// update current price every minute
+setInterval(function() {
+    console.log('updating latest...');
+    bitcoins.updateCurrent();
+}, 60000);
+
+// update averages every 30min
+setInterval(function() {
+    console.log('updating averages...');
+    bitcoins.updateAverages();
+}, 1800000);
+
---
+++ popup.js
@@ -0,0 +1,196 @@
+var BGP = chrome.extension.getBackgroundPage();
+var bitcoins = BGP.bitcoins;
+var month = 30 * 24 * 60 * 60;
+
+// loader
+$('#content').append('<img id="loading" src="loader.gif" />');
+
+// on load
+document.addEventListener('DOMContentLoaded', function () {
+
+    // grab bitcoin data from background
+    var data = bitcoins.getPrices();
+
+    // get historical data and draw the chart
+    bitcoins.queryHistory(drawChart);
+
+    // print details
+    printPrices(data);
+});
+
+
+function printPrices(updates) {
+
+    if(updates.currentPrice != -1 && typeof updates.currentPrice != 'undefined') {
+
+        $('#content').empty();
+        $('<div>').appendTo('#content')
+            .html('<b>BTC</b> <span class="money2">$</span>' + updates.currentPrice.toFixed(2))
+            .attr('id','currentPrice');
+
+        addUpdate('day',updates.day);
+        addUpdate('week',updates.week);
+        addUpdate('month',updates.month);
+    }
+
+}
+
+function addUpdate(text,data) {
+
+    var average = parseInt(data.average);
+    var div = $('<div class="sector">')
+        .css({color: color(data.change) })
+        .html('<div class="price"><span class="money">$</span>' + Math.abs(parseFloat(average.toFixed(2))) + '</div>'
+            + '<div class="priceLabel">' + text + '</div>')
+        .appendTo('#content');
+}
+
+// gets the sign of a value as 1,0,-1
+function sign(x) { return x ? x < 0 ? -1 : 1 : 0; }
+
+//gets the color based on the sign using the color globals for UP and DOWN defined in background.js
+function color(num) { return sign(num) > 0 ? BGP.UP : sign(num) < 0 ? BGP.DOWN : BGP.STAY; }
+
+function log(data) {
+    console.log(data);
+}
+
+// draws chart using the Highcharts library
+function drawChart(data) {
+
+    var prices = [];
+    for(var i = 0; i < data.values.length; i++) {
+        prices.push(parseInt(data.values[i]['y']));
+    }
+
+    var chart = new Highcharts.Chart({
+        chart: {
+            renderTo: $('#chart')[0],
+            zoomType: 'x',
+            spacingRight: 20,
+            backgroundColor:'rgba(255, 255, 255, 0)',
+            style: {
+                cursor: 'ew-resize'
+            },
+            resetZoomButton: {
+                theme: {
+                    display: 'none'
+                }
+            }
+        },
+        title: {
+            text: null
+        },
+        subtitle: {
+            text: null
+        },
+        xAxis: {
+            type: 'datetime',
+            maxZoom: 3 * 24 * 3600000, // fourteen days
+            title: {
+                text: null
+            },
+            lineWidth: 0,
+            minorGridLineWidth: 0,
+            lineColor: 'grey',
+            labels: {
+                enabled: true,
+                style: {
+                    color: 'white',
+                    fontSize: '8px'
+                }
+            },
+            minorTickLength: 0,
+            tickLength: 0
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: null
+            },
+            lineWidth: 0,
+            minorGridLineWidth: 0,
+            lineColor: 'transparent',
+            labels: {
+                enabled: true,
+                style: {
+                    color: 'white',
+                    fontSize: '8px'
+                }
+            },
+            minorTickLength: 0,
+            tickLength: 0,
+            gridLineColor: 'transparent'
+        },
+        drilldown: {
+            activeAxisLabelStyle: {
+                cursor: 'pointer',
+                color: 'red',
+                fontWeight: 'bold',
+                textDecoration: 'underline'
+            }
+        },
+        tooltip: {
+            shared: true,
+            style: {
+                color: 'black',
+                opacity: 1.0,
+                background: 'white',
+                fontSize: '12px',
+                textColor: 'black',
+                padding: '8px'
+
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                fillColor:'rgba(255, 255, 255, 0)',
+                lineWidth: 1,
+                marker: {
+                    enabled: false
+                },
+                shadow: false,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
+
+        series: [{
+            type: 'area',
+            name: 'USD',
+            pointInterval: 24 * 3600 * 1000,
+            pointStart: data.values[0]['x']*1000,
+            data: prices,
+            color: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 2},
+                    stops: [
+                        [0, 'white'],
+                        [1, 'white']
+                    ]
+                }
+        }]
+    });
+
+    $('tspan').text('');
+    $('#resetZoom').click(function() {
+        // console.log(chart);
+        // chart.xAxis.setExtremes((d - month*1000)/1000, d/1000);
+        // console.log(chart.series[0].color.stops[0][1]);
+        chart.zoomOut();
+    });
+
+    var d = new Date().getTime();
+    // chart.xAxis[0].setExtremes((d - month*1000)/1000, d/1000);
+    chart.xAxis[0].setExtremes((d - month), d);
+    // chart.showResetZoom();
+
+}
+
+
